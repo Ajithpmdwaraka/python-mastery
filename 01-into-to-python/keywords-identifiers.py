45f26cb5b39ebdd4251ae106704af6c20ccf335e
Ajith PM <ajithpmdwaraka@gmail.com>
2025-08-27 21:45:59 +0530

    keywords and identifiers

---
+++ 01-into-to-python/keywords-identifiers.py
@@ -0,0 +1,4 @@
+# Keywords are reserved words in python
+
+# Identifiers are the names given to variables, functions, classes, etc.
+
---
+++ 01-into-to-python/statement.py
@@ -0,0 +1,7 @@
+# Statements are instructions that a Python interpreter can execute,
+# such as assignments, function calls, loops, or conditional blocks.
+
+x = 10          # assignment statement
+print(x)        # function call statement
+if x > 5:       # conditional statement
+    print("x is greater than 5")
