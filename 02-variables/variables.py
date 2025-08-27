ee9c3d2412c1802cfabfaf8ba043f3ba64759ef7
Ajith PM <ajithpmdwaraka@gmail.com>
2025-08-27 22:11:12 +0530

    variables

---
+++ 02-variables/variables.py
@@ -0,0 +1,11 @@
+# variables are containers that hold data
+a = 4
+print(a)
+
+# Assigning multiple values to multiple variables in a single line
+b, c, d, e = 5, 6, 7, 8
+print(b, c, d, e)
+
+# Assigning the same value to multiple variables in a single line
+x = y = z = 9
+print(x, y, z)
