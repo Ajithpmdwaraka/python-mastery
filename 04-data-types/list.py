7c4ea9405ab78f1f9b5507fd1ccd1b8442315fdf
Ajith PM <ajithpmdwaraka@gmail.com>
2025-08-28 17:36:50 +0530

    list

---
+++ 04-data-types/list.py
@@ -0,0 +1,9 @@
+# Example of a list
+fruits = ["apple", "banana", "cherry", 10, True]
+
+# Lists are mutable
+fruits[1] = "mango"  # changes "banana" to "mango"
+print(fruits)  # ['apple', 'mango', 'cherry', 10, True]
+
+print(fruits[0:2])
+print(fruits[1:])
