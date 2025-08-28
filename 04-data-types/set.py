76bbc8fa200fcff371c50b03c8ec4959064d23de
Ajith PM <ajithpmdwaraka@gmail.com>
2025-08-28 17:50:03 +0530

    set

--- 04-data-types/set.py
+++ 04-data-types/set.py
@@ -1,1 +1,7 @@
 # Set is an unordered collection of unique items
+
+numbers = {1, 2, 3, 2, 1}
+
+print(numbers)        # {1, 2, 3} (duplicates removed)
+numbers.add(4)        # adding element
+print(numbers)        # {1, 2, 3, 4}
