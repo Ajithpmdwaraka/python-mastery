a432eeb9a4b7becae9fb99a7592c45aa1a24d1e4
Ajith PM <ajithpmdwaraka@gmail.com>
2025-08-29 18:43:01 +0530

    comparison

---
+++ 06-operators/comparison-operator.py
@@ -0,0 +1,9 @@
+x = 5
+y = 10
+
+print(x == y)   # Equal → False
+print(x != y)   # Not equal → True
+print(x > y)    # Greater → False
+print(x < y)    # Less → True
+print(x >= y)   # Greater or equal → False
+print(x <= y)   # Less or equal → True
