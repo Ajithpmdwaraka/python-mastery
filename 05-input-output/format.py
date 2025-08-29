4f52f4c4c2b3e9bca9f1674dc47d601e098b6aa1
Ajith PM <ajithpmdwaraka@gmail.com>
2025-08-29 06:12:16 +0530

    format function

---
+++ 05-input-output/format.py
@@ -0,0 +1,8 @@
+x = 20
+y = 40
+print("This is the value of x {} and the value of y is {}".format(x, y))
+
+print("This is the value of x {1} and the value of y is {0}".format("Milk", "Tea"))
+
+# Using keyword arguments in the format() method
+print("This is the value of x {x} and the value of y is {y}".format(x="Milk", y="Tea"))
