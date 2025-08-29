aff8943717eb0b97ca4e107baaa9aa8b457d4095
Ajith PM <ajithpmdwaraka@gmail.com>
2025-08-29 19:08:09 +0530

    membership-operator

--- 06-operators/arithmetic-operators.py
+++ 06-operators/arithmetic-operators.py
@@ -4,7 +4,7 @@ b = 3
 print(a + b)   # Addition → 13
 print(a - b)   # Subtraction → 7
 print(a * b)   # Multiplication → 30
-print(a / b)   # Division → 3.333...
-print(a // b)  # Floor Division → 3
+print(a / b)   # Division → 3.333... will get an floating point number
+print(a // b)  # Floor Division → 3 will get an integer number, avoids decimal part
 print(a % b)   # Modulus → 1 (remainder)
 print(a ** b)  # Exponentiation → 1000 (10^3)
---
+++ 06-operators/membership-operators.py
@@ -0,0 +1,4 @@
+fruits = ["apple", "banana", "cherry"]
+
+print("apple" in fruits)     # True
+print("mango" not in fruits) # True
