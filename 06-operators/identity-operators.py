ff68a65cc87f6c2cde2b7e80b12f0e9a3dcc21e0
Ajith PM <ajithpmdwaraka@gmail.com>
2025-08-29 19:59:00 +0530

    identity operator

---
+++ 06-operators/identity-operators.py
@@ -0,0 +1,9 @@
+# Used to check if two variables point to the same object (memory).
+
+x = [1, 2, 3]
+y = [1, 2, 3]
+z = x
+
+print(x is y)     # False (different objects in memory)
+print(x is z)     # True  (same object in memory)
+print(x is not y) # True
--- 06-operators/membership-operators.py
+++ 06-operators/membership-operators.py
@@ -1,3 +1,4 @@
+# Used to check if a value exists in a sequence (string, list, tuple, etc.).
 fruits = ["apple", "banana", "cherry"]
 
 print("apple" in fruits)     # True
