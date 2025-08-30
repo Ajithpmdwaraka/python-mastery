52e28a6d39159d6b128cf8b90ebf06679e8bfc29
Ajith PM <ajithpmdwaraka@gmail.com>
2025-08-30 19:30:42 +0530

    if-else-else

---
+++ 07-conditional-statement.py/if-elif-else.py
@@ -0,0 +1,10 @@
+marks = 90
+
+if marks >= 90:
+    print("Your grade is A+")
+elif marks >= 70:
+    print("Your grade is A")
+elif marks >= 50:
+    print("Your grade is B")
+else:
+    print("Your grade is C")
