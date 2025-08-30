42bc3f588eace2d3d56a8b823da4bdd397859dfb
Ajith PM <ajithpmdwaraka@gmail.com>
2025-08-30 19:16:24 +0530

    if-statement

--- 06-operators/identity-operators.py
+++ 06-operators/identity-operators.py
@@ -4,6 +4,6 @@ x = [1, 2, 3]
 y = [1, 2, 3]
 z = x
 
-print(x is y)     # False (different objects in memory)
-print(x is z)     # True  (same object in memory)
-print(x is not y) # True
+print(x is y)  # False (different objects in memory)
+print(x is z)  # True  (same object in memory)
+print(x is not y)  # True
--- 06-operators/membership-operators.py
+++ 06-operators/membership-operators.py
@@ -1,5 +1,8 @@
 # Used to check if a value exists in a sequence (string, list, tuple, etc.).
 fruits = ["apple", "banana", "cherry"]
 
-print("apple" in fruits)     # True
-print("mango" not in fruits) # True
+words = "Hey"
+
+print("apple" in fruits)  # True
+print("mango" not in fruits)  # True
+print("H" in words)  # True
