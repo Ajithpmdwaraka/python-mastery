67304ec15ffd14006e18849b681a0a010b85e739
Ajith PM <ajithpmdwaraka@gmail.com>
2025-08-31 09:15:18 +0530

    while-loop

---
+++ 07-conditional-statement.py/nested-if.py
@@ -0,0 +1,11 @@
+number = 15
+
+if number > 0:
+    print("The number is positive")
+    
+    if number % 2 == 0:
+        print("It is also even")
+    else:
+        print("It is also odd")
+else:
+    print("The number is not positive")
